4c16072225e5b3f55e57bf069218d2ce490a428e
Roman Vabishchevych <roma@se7ensky.com>
2017-08-31 20:34:39 +0300

    dog-block animation

---
+++ src/blocks/dog-block/dog-block.js
@@ -0,0 +1,16 @@
+$(function() {
+	var $block = $('.dog-block');
+	var $link = $block.find('.dog-block__link');
+
+	$link.each(function(){
+		var $this = $(this);
+		var $linkIcon = $this.find('.icon')
+
+		$this.hover(function(){
+			TweenMax.to($linkIcon, 1, {x: 10, ease: Elastic.easeOut.config(1, 0.3)});
+		}, function(){
+			TweenMax.to($linkIcon, .7, {x: 0, ease: Power2.easeIn});
+		});	
+	})
+
+})
